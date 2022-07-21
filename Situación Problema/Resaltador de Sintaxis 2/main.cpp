f9490e1cd213833b1cb7f94ad27f5c80203b27a9
Caceres-A01706972 <A01706972@tec.mx>
2022-07-21 15:21:20 -0600

    Adds input files

---
+++ Situación Problema/Resaltador de Sintaxis 2/main.cpp
@@ -0,0 +1,38 @@
+// =================================================================
+//
+// ACTIVIDAD INTEGRADORA 5.3 RESALTADOR DE SINTAXIS PARALELO
+// Author: Ricardo Andres Caceres Villibord A01706972
+// Description: Resaltador de Sintaxis de C++ utilizando MULTIHILOS
+//
+// Copyright (c) 2020 by Tecnologico de Monterrey.
+// All Rights Reserved. May be reproduced for any non-commercial
+// purpose.
+//
+// Github:
+// =================================================================
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+#include <cstring>
+#include <string>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <regex>
+#include "utils.h"
+
+const int HILOS = 8; //Threads to be used
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "You have entered " << argc
+         << " arguments:" << "\n";
+  
+    for (int i = 0; i < argc; ++i)
+        cout << argv[i] << "\n";
+  
+    return 0;
+}
