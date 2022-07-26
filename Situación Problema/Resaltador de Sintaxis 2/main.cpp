6026fe673ef8e48827f79c429aaef20a5a1ad3d0
Caceres-A01706972 <A01706972@tec.mx>
2022-07-26 17:22:50 -0600

    Adds Concurrent

--- Situación Problema/Resaltador de Sintaxis 2/main.cpp
+++ Situación Problema/Resaltador de Sintaxis 2/main.cpp
@@ -121,13 +121,13 @@ void resaltadorSec(string *rutas, int size){
             
             vectorPalabras = llenarVectorPalabras(codigo, vectorPalabras); 
             
-            for(int i = 0; i < vectorPalabras.size(); i++){
-                cout<<vectorPalabras[i]<<endl;
-            }
+            // for(int i = 0; i < vectorPalabras.size(); i++){
+            //     cout<<vectorPalabras[i]<<endl;
+            // }
 
             //Esto imprime ResaltadorSecuencial_#1.html
             string outFile_name = "ResaltadorSecuencial_#" + rutas[i].substr(rutas[1].length() - 5, 1) + ".html";
-            cout<<"\n Nombre del archivo: "<<outFile_name<<endl;
+            // cout<<"\n Nombre del archivo: "<<outFile_name<<endl;
 
             ofstream htmlOutput(outFile_name);
 
@@ -188,10 +188,97 @@ void resaltadorSec(string *rutas, int size){
     }
 }
 
-int main(int argc, char** argv){
+//Estructura de Bloque
+typedef struct {
+    int start, end;
+    string* arreglo;
+} Block;
 
+void* resaltadorConcurrente(void* params){
+    vector<string> vectorPalabras;
+
+    Block *block;
+    block = (Block*) params;
+
+    for(int i = block->start; i < block->end; i++){
+        string line;
+        string codigo;
+        ifstream inputFile(block->arreglo[i]); //reads file
+
+        if(inputFile.is_open()){
+            while(getline(inputFile, line)){
+                codigo += line;
+                codigo += "\n";
+            }
+
+            vectorPalabras = llenarVectorPalabras(codigo, vectorPalabras); 
+
+            //Esto imprime ResaltadorSecuencial_#1.html
+            string outFile_name = "Resaltador_Concurrente_#" + block->arreglo[i].substr(block->arreglo[1].length() - 5, 1) + ".html";
+
+            ofstream htmlOutput(outFile_name);
+
+            htmlOutput << "<!DOCTYPE html> \n";
+            htmlOutput << "<html lang = 'es'> \n";
+            htmlOutput << "<head> \n\t";
+            htmlOutput << "<meta charset = 'UTF-8'> \n\t";
+            htmlOutput << "<meta http - equiv = 'X-UA-Compatible' content = 'IE=edge'>\n\t";
+            htmlOutput << "<meta name = 'viewport' content = 'width=device-width, initial-scale=1.0'>\n\t";
+            htmlOutput << "<title> Secuencial "<<block->arreglo[i].substr(block->arreglo[1].length() - 5, 1)<<"</title> <link rel = 'stylesheet' href = 'styles.css'>";
+            htmlOutput << "</head>\n<body>\n";
+
+            for(int posPalabra = 0; posPalabra < vectorPalabras.size(); posPalabra++){
+                string palabra = vectorPalabras[posPalabra];
+                if(isInclude(palabra)){
+                    regex lessThan("<");
+                    regex greaterThan(">");
+                    string word = regex_replace(palabra, lessThan, "&lt;");
+                    word = regex_replace(word, greaterThan, "&gt;");
+                    htmlOutput << "<span class='include'>" << word << "</span>";
+                } else if(isFromLanguage(palabra)){
+                    htmlOutput << "<span class='reserved'>" << palabra << "</span>";
+                } else if(isNumero(palabra)){
+                    htmlOutput << "<span class='numero'>" << palabra << "</span>";
+                } else if(isVariable(palabra)){
+                    htmlOutput << "<span class='variable'>" << palabra << "</span>";
+                } else if(isOperador(palabra)){
+                    regex lessThan("<");
+                    regex greaterThan(">");
+                    string word = regex_replace(palabra, lessThan, "&lt;");
+                    word = regex_replace(word, greaterThan, "&gt;");
+                    htmlOutput << "<span class='operador'>" << word << "</span>";
+                } else if(isComment(palabra)){
+                    htmlOutput << "<span class='comment'>" << palabra << "</span>";
+                } else if(isCadena(palabra)){
+                    htmlOutput << "<span class='string'>" << palabra << "</span>";
+                } else if(isEspacio(palabra)){
+                    htmlOutput << " ";
+                } else if(isSalto(palabra)){
+                    htmlOutput << "\n"; 
+                    htmlOutput << "<br>";
+                } else if(isTab(palabra)){
+                    htmlOutput << "\t";
+                    htmlOutput << "&emsp;";
+                } else if(isEncierra(palabra)){
+                    htmlOutput << "<span class='encierra'>" << palabra << "</span>";
+                } else if(isPuntuacion(palabra)){
+                    htmlOutput << "<span class='puntuacion'>" << palabra << "</span>";
+                }
+            }
+
+            htmlOutput << "\n</body>\n</html>";
+            htmlOutput.close();
+        }
+    }
+    pthread_exit(0);
+}
+
+int main(int argc, char** argv){
     double sequential, concurrent; // Tiempo en ms
     string *rutasArchivos;
+    pthread_t hilos[HILOS];
+    Block blocks[HILOS];
+    int blockSize;
 
     const int arraySize = argc;
 
@@ -201,26 +288,48 @@ int main(int argc, char** argv){
         rutasArchivos[i] = argv[i];
     }
     
-    // for(int i = 0; i < arraySize; i++){
-    //     cout<<rutasArchivos[i]<<endl;
-    // }
-
 //========================SEQUENTIAL========================================================
-    cout << "SECUENCIAL: \n" << endl;
+    cout << "\nEJECUTANDO DE MANERA SECUENCIAL: " << endl;
     sequential = 0;
     start_timer();
     resaltadorSec(rutasArchivos, arraySize);
     sequential += stop_timer();
 
-    cout << "Sequential Time = " << setprecision(5) << sequential << " ms" << endl;
+    cout << "Sequential Time = " << setprecision(5) << sequential << " ms\n" << endl;
 //==========================================================================================
 
+//========================CONCURRENT========================================================
+    blockSize = arraySize / HILOS;
 
-    cout << "You have entered " << argc
-         << " arguments:" << "\n";
+    for(int i = 0; i < HILOS; i++){
+        blocks[i].start = i * blockSize;
+        blocks[i].end = (i != (HILOS - 1))? (i + 1) * blockSize : arraySize;
+        blocks[i].arreglo = rutasArchivos;
+    }
+
+    cout << "EJECUTANDO DE MANERA CONCURRENTE: " << endl;
+    concurrent = 0;
+    start_timer();
+    for(int i = 0; i < HILOS; i++){
+        pthread_create(&hilos[i], NULL, resaltadorConcurrente, (void*)&blocks[i]);
+    }
+
+    for(int i = 0; i < HILOS; i++){
+        pthread_join(hilos[i], NULL);
+    }
+    concurrent += stop_timer();
+    cout << "Concurrent Time = " << setprecision(5) << concurrent << " ms\n" << endl;
+//==========================================================================================
+
+    cout << "SPEED UP = " << setprecision(5) << (sequential / concurrent) << " ms\n" << endl;
+
+    delete [] rutasArchivos;
+
+    // cout << "You have entered " << argc
+    //      << " arguments:" << "\n";
   
-    for (int i = 0; i < argc; ++i)
-        cout << "FILE #" << i << ": " << argv[i] << "\n";
+    // for (int i = 0; i < argc; ++i)
+    //     cout << "FILE #" << i << ": " << argv[i] << "\n";
   
     return 0;
 }
